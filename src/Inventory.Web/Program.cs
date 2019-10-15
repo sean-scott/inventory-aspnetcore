0986219116e46b65502aebc81fefbe27a586c3db
Sean Scott <sean.scott894@gmail.com>
2019-10-15 13:36:14 -0700

    Moving things around

--- src/Inventory.Web/Program.cs
+++ src/Inventory.Web/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Inventory
+namespace Inventory.Web
 {
     public class Program
     {
--- src/Inventory.Web/Startup.cs
+++ src/Inventory.Web/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Inventory
+namespace Inventory.Web
 {
     public class Startup
     {
